07400602ec43f3c2faf5ccc716c903dcce573bce
João Victor <joa9718@gmail.com>
2025-05-22 23:57:09 -0300

    Bootstrap e Blade

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+// import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default {
+    content: [
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+      './storage/framework/views/*.php',
+      './resources/views/**/*.blade.php',
+      './resources/js/**/*.js',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        },
+      },
+    },
+
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        // tailwindcss(),
     ],
-});
+}
+
