ca29d7cbf21624a7bf8fc43471d86a007f2726c1
João Victor <joa9718@gmail.com>
2025-05-26 23:43:59 -0300

    Simplificando as rotas e utilizando fillable

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -21,12 +21,9 @@ class SeriesController extends Controller
 
     public function store(Request $request){
         
-        $nomeSerie = $request->input("nome");
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
+        Serie::create($request->all());
 
-        return redirect("/series");
+        return to_route('series.index');
 
     }
 
